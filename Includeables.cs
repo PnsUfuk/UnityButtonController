d7e110f120269e907097908794ed9c1c70ffc54a
PnsUfuk <pns.ufuk@gmail.com>
2021-03-08 18:16:40 +0300

    Add files via upload

---
+++ Includeables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Include
+{
+    public enum PockState { Free, Owned };
+    public enum PlayerState { Free,HasPock };
+
+    public enum ButtonState {non , Down , Up , Press};
+}
